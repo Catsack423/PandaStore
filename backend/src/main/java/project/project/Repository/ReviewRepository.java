c9a734964a2c46e7c2d43b5ae013878544f9e42a
Nattapong Gontanagit <nattapong.g@kkumail.com>
2026-09-09 21:04:37 +0700

    feat(repo): add review and reply query methods

--- backend/src/main/java/project/project/Repository/ReviewRepository.java
+++ backend/src/main/java/project/project/Repository/ReviewRepository.java
@@ -3,6 +3,12 @@ package project.project.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.project.Entity.review.Review;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByProduct_ProductId(Long productId);
+    List<Review> findByProduct_ProductIdOrderByCreatedAtDesc(Long productId);
+    Optional<Review> findByOrderItem_OrderItemId(Long orderItemId);
+    boolean existsByOrderItem_OrderItemId(Long orderItemId);
 }
