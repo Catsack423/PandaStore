15ed4e6940384497aecf89f9366c2af33b5d5ec5
Nattapong Gontanagit <nattapong.g@kkumail.com>
2026-09-09 21:04:40 +0700

    feat(repo): add seller application and seller queries

--- backend/src/main/java/project/project/Repository/SellerApplicationRepository.java
+++ backend/src/main/java/project/project/Repository/SellerApplicationRepository.java
@@ -2,7 +2,11 @@ package project.project.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.project.Entity.seller.SellerApplication;
+import project.project.Entity.seller.SellerApplicationStatus;
+
+import java.util.List;
 
 public interface SellerApplicationRepository extends JpaRepository<SellerApplication, Long> {
-
+    List<SellerApplication> findByStatus(SellerApplicationStatus status);
+    List<SellerApplication> findByUser_UserId(Long userId);
 }
--- backend/src/main/java/project/project/Repository/SellerRepository.java
+++ backend/src/main/java/project/project/Repository/SellerRepository.java
@@ -3,6 +3,9 @@ package project.project.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.project.Entity.seller.Seller;
 
-public interface SellerRepository extends JpaRepository<Seller, Long> {
+import java.util.Optional;
 
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+    Optional<Seller> findByUser_UserId(Long userId);
+    boolean existsByShopName(String shopName);
 }
